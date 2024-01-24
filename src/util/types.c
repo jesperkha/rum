11595d9ad445e06593476a11178a5af8bd9d741e
jesperkha <hammerjesp@gmail.com>
2024-01-24 23:22:53 +0100

    lists

---
+++ src/util/types.c
@@ -0,0 +1,71 @@
+#include "wim.h"
+
+typedef enum Type
+{
+    T_LIST,
+} Type;
+
+typedef struct TypeHeader
+{
+    Type type;
+
+    int length;
+    int cap;
+    int dataSize;
+} TypeHeader;
+
+#define HEADER_SIZE (sizeof(TypeHeader))
+
+static TypeHeader *getHeader(void *ptr)
+{
+    return (TypeHeader*)(ptr - HEADER_SIZE);
+}
+
+// Implementation of 'dynamic array'. Allows direct index access while
+// also doing reallocation and stuff in the background.
+
+// Returns pointer to new List
+void *ListCreate(size_t dataSize, size_t length)
+{
+    TypeHeader header = {
+        .type = T_LIST,
+        .length = 0,
+        .cap = length,
+        .dataSize = dataSize,
+    };
+
+    void *ptr = memZeroAlloc(HEADER_SIZE + (dataSize * length));
+    memcpy(ptr, &header, HEADER_SIZE);
+    return (ptr + HEADER_SIZE);
+}
+
+// Returns length of list. Only valid if appended to with ListAppend or append.
+int ListLen(void *list)
+{
+    return getHeader(list)->length;
+}
+
+// Appends item to end of list. Fails if full.
+void ListAppend(void *list, superlong item)
+{
+    TypeHeader *header = getHeader(list);
+    int size = header->dataSize;
+
+    if (header->length < header->cap)
+    {
+        int length = (header->length * size);
+
+        if (size > sizeof(superlong))
+            memcpy(list + length, (void*)item, size);
+        else
+            memcpy(list + length, &item, size);
+
+        header->length++;
+    }
+}
+
+// Frees array and type header.
+void ListFree(void *list)
+{
+    memFree(list - HEADER_SIZE);
+}
--- src/util/util.h
+++ src/util/util.h
@@ -12,3 +12,14 @@ void *memAlloc(int size);
 void *memZeroAlloc(int size);
 void *memRealloc(void *ptr, int newSize);
 void memFree(void *ptr);
+
+#define superlong unsigned long long int
+
+void *ListCreate(size_t dataSize, size_t length);
+int ListLen(void *list);
+void ListAppend(void *list, superlong item);
+void ListFree(void *list);
+
+#define List(T, size) (T*)ListCreate(sizeof(T), size)
+#define len(list) (ListLen(list))
+#define append(list, item) ListAppend(list, (superlong)item)
