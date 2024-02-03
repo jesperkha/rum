9f1b3de45a07c4dbf1badf4fefff7ea0d9eba496
jesperkha <hammerjesp@gmail.com>
2024-02-03 17:33:49 +0100

    removed scaffold

--- src/editor/editor.c
+++ src/editor/editor.c
@@ -278,8 +278,7 @@ Status EditorHandleInput()
                 break;
             
             case 'r':
-                // AppendEditAction(A_DELETE, editor.row, editor.col, "Hello");
-                // AppendEditAction(A_WRITE, editor.row, editor.col, "12345");
+                Redo();
                 break;
 
             case 'q':
