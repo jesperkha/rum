3864f2cec7dcac5596f3e841f68dd62ad147c969
jesperkha <hammerjesp@gmail.com>
2024-08-21 12:37:49 +0200

    plugin util

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 #include "rum.h"
 
+// Todo: Select and multi-line select
+// Todo: C multi-line comments
+// Todo: Run terminal commands from editor
+// Todo: Comment out line/selection
+// Todo: Save config changes at runtime (theme etc)
+
 int main(int argc, char **argv)
 {
     CmdOptions options = ProcessArgs(argc, argv);
---
+++ src/util/plugin.c
@@ -0,0 +1,48 @@
+#include "rum.h"
+
+#include <windows.h>
+#include <string.h>
+
+typedef void (*PlugInitFunc)(void);
+
+// Loads plugin DLLs and puts init functions into results array.
+// Returns number of results. Returns -1 on error.
+int LoadPlugins(char *pluginDir, PlugInitFunc *results, size_t maxResults)
+{
+    SetCurrentDirectory(pluginDir);
+
+    WIN32_FIND_DATAA data;
+    HANDLE hfind = FindFirstFileA("./*", &data);
+    if (hfind == INVALID_HANDLE_VALUE)
+        return -1;
+
+    int count = 0;
+
+    do
+    {
+        char *filename = data.cFileName;
+        size_t length = strlen(filename);
+
+        if (length <= 4 || strcmp(filename + (length - 4), ".dll"))
+            continue;
+
+        HMODULE hplugin = LoadLibraryA(filename);
+        if (hplugin == NULL)
+        {
+            Errorf("Failed to load plugin %s", filename);
+            continue;
+        }
+
+        PlugInitFunc f = (PlugInitFunc)GetProcAddress(hplugin, "PlugInit");
+        if (f == NULL)
+        {
+            Errorf("Cannot find init func in plugin %s", filename);
+            continue;
+        }
+
+        results[count] = f;
+        count++;
+    } while (FindNextFileA(hfind, &data) != 0 && count < maxResults);
+
+    return count;
+}
