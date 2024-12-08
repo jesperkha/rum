aaf5a6ae8e59d1a392c5b62edfb56e65066225c8
jesperkha <hammerjesp@gmail.com>
2024-12-08 21:55:11 +0100

    fix publish script

--- scripts/publish_release.sh
+++ scripts/publish_release.sh
@@ -1,9 +1,9 @@
 make installer
 powershell -File scripts/zip_installer.ps1
 
-read -p "Enter tag name: vx.x.x" tag
-
 python scripts/download_button.py
+
+read -p "Enter tag name: vx.x.x: " tag
 
 git add .
 git commit -m "$tag"
@@ -11,7 +11,4 @@ git push origin dev
 
 notes=$(sed '/<br>/q' changelog.md | sed '/<br>/d')
 
-gh release create $tag --draft \
-    --title "rum $tag" \ 
-    --notes "$notes" \
-    dist/RumInstaller.zip
+gh release create "$tag" --draft --title "rum $tag" --notes "$notes" dist/RumInstaller.zip
